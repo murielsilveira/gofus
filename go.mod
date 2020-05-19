d5ac26514a775f38a95799dff9b9c09cf52c6664
Muriel Silveira <murielkong@gmail.com>
2020-05-20 01:26:03 +0200

    chore: config postgres

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/murielsilveira/gofus
 
 go 1.14
 
-require github.com/gofiber/fiber v1.9.6 // indirect
+require (
+	github.com/gofiber/fiber v1.9.6
+	github.com/lib/pq v1.5.2
+)
--- server.go
+++ server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/gofiber/fiber"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -11,11 +15,28 @@ func main() {
 	if port == "" {
 		port = "9000"
 	}
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		dbURL = "postgres://postgres:postgres@localhost/?sslmode=disable"
+	}
 
 	app := fiber.New()
 
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
+	}
+
 	app.Get("/", func(c *fiber.Ctx) {
 		c.Send("Hello, world!")
+	})
+
+	app.Get("/db", func(c *fiber.Ctx) {
+		if _, err := db.Exec("SELECT 1"); err != nil {
+			c.Send(fmt.Sprintf("Error querying database: %q", err))
+		} else {
+			c.Send("WORKED!!")
+		}
 	})
 
 	app.Listen(port)
